4487d21cb8bd5c99e42da19ed78b94ad831d23b6
lt1029re <lt1029re@ed.ritsumei.ac.jp>
2019-11-27 05:16:34 +0000

    課題６－２を追加

--- kadai-6/index2.js
+++ kadai-6/index2.js
@@ -11,4 +11,50 @@ const str6 = '{a: 123}'
 // 正しくは　"abc"
 
 console.log(JSON.stringify(str1))
+try{
+    const str1='{"a":123,'
+    JSON.parse(str1)
+}catch (arror){
+    console.error(error)
+    //JSONとして有効でないため
+}
 console.log(JSON.stringify(str2))
+try{
+    const str2='{"a":123,'
+    JSON.parse(str2)
+}catch (arror){
+    console.error(error)
+    //abc という文字列は正しい　JSONではないから　正しくは"abc"
+}
+console.log(JSON.stringify(str3))
+try{
+    const str3='{"a":123,'
+    JSON.parse(str3)
+}catch (arror){
+    console.error(error)
+    //JSONとして有効でないため
+}
+console.log(JSON.stringify(str4))
+try{
+    const str4='{"a":123,'
+    JSON.parse(str4)
+}catch (arror){
+    console.error(error)
+    //JSONとして有効でないため
+}
+console.log(JSON.stringify(str5))
+try{
+    const str5='{"a":123,'
+    JSON.parse(str5)
+}catch (arror){
+    console.error(error)
+    //JSONとして有効でないため
+}
+console.log(JSON.stringify(str6))
+try{
+    const str6='{"a":123,'
+    JSON.parse(str6)
+}catch (arror){
+    console.error(error)
+    //JSONとして有効でないため
+}
